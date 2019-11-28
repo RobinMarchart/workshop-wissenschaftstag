061d204094a697e69eacbf8ebb96bc207f758d4b
Robin Marchart <Robin.Marchart@hotmail.com>
2019-11-29 00:08:10 +0100

    plans

--- survey/src/index.ts
+++ survey/src/index.ts
@@ -1,9 +1,26 @@
-import {  } from "express";
-export class C {
-    private x = 10
-    getX = () => this.x;
-    setX = (newVal: number) => { this.x = newVal; }
-}
+import * as http from "http";
+import * as https from "https";
+import express from "express";
+let app=express();
+/*
 
-export let x = new C();
-export let y = { ...{ some: "value" } }
+GET / -> single page app html
+
+GET /static -> static, bundled and compiled resources
+
+GET /t -> add survey -> redirect to / with query string
+
+PUT /join -> send id, register and receive survey link
+
+PUT /survey -> send id and receive survey questions and (encrypted) answers
+
+POST /answer -> send id and (encrypted) answer
+
+*/
+function serve(use_https:boolean) {
+    if(use_https){
+        https.createServer(app).listen(443);
+    }else{
+        http.createServer(app).listen(80);
+    }
+    }
